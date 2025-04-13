0eff67752e516fe24dfa59a8cbb6409ab74c9b3e
Akhilesh Yadav <130656190+akhil0778@users.noreply.github.com>
2025-04-13 21:30:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  
     allowedHeaders: ['Content-Type', 'Authorization'] 
   }));
